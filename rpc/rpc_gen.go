df9122a583e13a1f6a378df6185c1cf88729866c
Greg Poirier <greg.istehbest@gmail.com>
2018-11-24 17:45:16 -0800

    rpc fixes
    Signed-off-by: Greg Poirier <greg.istehbest@gmail.com>

--- rpc/rpc_gen.go
+++ rpc/rpc_gen.go
@@ -1,5 +1,5 @@
 package rpc
 
 //go:generate go install github.com/sensu/sensu-go/vendor/github.com/golang/protobuf/protoc-gen-go
-//go:generate -command protoc protoc -I ../../../../ -I . -I ../types/ -I ../vendor/ --go_out=plugins=grpc:.
+//go:generate -command protoc protoc -I ../../../../ -I . -I ../api/core/v2/ -I ../vendor/ --go_out=plugins=grpc:.
 //go:generate protoc extension.proto
